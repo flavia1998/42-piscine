0d0235fce4b87754fb59526488731ff19e71039d
flavia1998 <118297933+flavia1998@users.noreply.github.com>
2023-02-22 10:32:51 +0000

    Delete ft_matrix.h

--- ft_matrix.h
+++
@@ -1,6 +0,0 @@
-void ft_map_numbers(int numbers[16], char *str);
-int ft_is_args_valid(int argc, char **argv);
-int ft_validate_matrix(int table[4][4], int position, const int set[16]);
-void ft_print_matrix(int matrix[4][4]);
-int ft_is_double(int table[4][4], int position, int size);
-int ft_solve_matrix(int table[4][4], const int numbers[16], int position);
